bedfca7c613676c9469c4c4f73db676436413739
Jesse Fowler <fowler.514@buckeyemail.osu.edu>
2021-09-22 23:19:22 -0400

    Implement base abstract MarioState class.

---
+++ States/MarioState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game1
+{
+    abstract class MarioState
+    {
+        protected AvatarSprite Sprite { get; set; }
+        public abstract void MoveLeft();
+        public abstract void MoveRight();
+        public abstract void Jump();
+        public abstract void Crouch();
+        public abstract void DashOrThrowFireball(); // Future command
+    }
+}
