1be04a68906a3f454511fa21a34945db6c4d0dc6
Jesse Fowler <fowler.514@buckeyemail.osu.edu>
2021-09-23 18:51:24 -0400

    Created Mario class containing Sprite and both States.

---
+++ Mario/Mario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game1
+{
+    class Mario
+    {
+        ISprite sprite;
+        MarioAction actionState;
+        MarioPower powerState;
+    }
+}
