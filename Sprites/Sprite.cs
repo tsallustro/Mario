eb7fe631fc1d0e19a6c5382b8f14d5ec3dec60a2
Jesse Fowler <fowler.514@buckeyemail.osu.edu>
2021-09-27 18:21:58 -0400

    Fix sprite animation speed.

--- Sprites/Sprite.cs
+++ Sprites/Sprite.cs
@@ -48,7 +48,7 @@ namespace Sprites
         // Sets Frame Animation. @param int initialFrame int finalFrame
         public void Animation()
         {
-            if (timer > 35)
+            if (timer > 6)
             {
                 currentFrame++;
                 if (currentFrame == FinalFrame + 1)
