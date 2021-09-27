70dbeee17e6979ebb3d15f4bb260cd196ee85875
Jesse Fowler <fowler.514@buckeyemail.osu.edu>
2021-09-26 22:23:06 -0400

    Created Block class.

---
+++ GameObjects/Block.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sprites;
+
+namespace CornetGame.GameObjects
+{
+    class Block
+    {
+        ISprite sprite;
+        //IBlockState state;
+
+        public Block(ISprite sprite)
+        {
+            this.sprite = sprite;
+            //this.state = state;
+        }
+    }
+}
