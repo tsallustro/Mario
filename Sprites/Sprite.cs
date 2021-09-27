2f7d3394007771e7d0d60980e23d03d298259560
Jesse Fowler <fowler.514@buckeyemail.osu.edu>
2021-09-27 18:53:51 -0400

    Fixed error caused by not loading textures into goomba sprite factory.

--- Sprites/Sprite.cs
+++ Sprites/Sprite.cs
@@ -37,7 +37,7 @@ namespace Sprites
 
         public void Update()
         {
-            //Animation();
+            Animation();
             //ChangeDirection();
             //Move();
             //Jump();
