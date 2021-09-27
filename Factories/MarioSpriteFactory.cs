99af5cb82edc97d89666118ef57680d580558868
Jesse Fowler <fowler.514@buckeyemail.osu.edu>
2021-09-26 23:16:06 -0400

    Begin implementing Mario Sprite Factory.

--- Factories/MarioSpriteFactory.cs
+++ Factories/MarioSpriteFactory.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
-namespace CornetGame.Factories
+namespace Factories
 {
-    class MarioSpriteFactory
+    public class MarioSpriteFactory
     {
-    }
+        private Texture2D marioSpritesheet;
+
+		private static MarioSpriteFactory factoryInstance = new MarioSpriteFactory();
+
+		public static MarioSpriteFactory Instance
+		{
+			get
+			{
+				return factoryInstance;
+			}
+		}
+
+		private MarioSpriteFactory()
+		{
+		}
+
+		public void LoadTextures(Game game)
+		{
+			marioSpritesheet = game.Content.Load<Texture2D>("Mario");
+		}
+	}
 }
