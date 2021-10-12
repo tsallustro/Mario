5dbbf388d9f481b2481eb0b111578439ac0d5a00
Jesse Fowler <fowler.514@buckeyemail.osu.edu>
2021-10-12 15:44:11 -0400

    Removed to-do comment in Items.

--- GameObjects/Items.cs
+++ GameObjects/Items.cs
@@ -7,11 +7,6 @@ using States;
 
 namespace GameObjects
 {
-    /*
-     * Since Items have different effects, we will likely need separate
-     * concrete classes that extend this class, and we can make this into
-     * an abstract class.
-     */
     public abstract class Item : GameObject, IItem
     {
         protected readonly int boundaryAdjustment = -10;
