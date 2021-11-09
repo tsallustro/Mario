0803fb0c640daa4be4b69897a874398ec3e76366
Tae Kim <kim.5114@buckeyemail.osu.edu>
2021-11-09 13:46:10 -0500

    flag edit. haven't figured out yet. just little edit

---
+++ Factories/FlagSpriteFactory.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Sprites;
+using States;
+
+namespace Factories
+{
+	public class FlagSpriteFactory
+	{
+		private Texture2D flagSprites;
+
+		private static FlagSpriteFactory factoryInstance = new FlagSpriteFactory();
+
+		private ISprite flag;
+		private ISprite endingFlag;
+
+		public static FlagSpriteFactory Instance
+		{
+			get
+			{
+				return factoryInstance;
+			}
+		}
+
+		private FlagSpriteFactory()
+		{
+		}
+
+		public void LoadTextures(Game game)
+		{
+			flagSprites = game.Content.Load<Texture2D>("Flagx50");
+		}
+		public ISprite Createflag(Vector2 location)
+		{
+			if(flag == null)
+            {
+				flag = new Sprite(false, true, location, flagSprites, 1, 50, 0, 0);
+			    return flag;
+			}
+			else return flag;
+		}
+		public ISprite CreateEndingFlag(Vector2 location)
+		{
+			if (endingFlag == null)
+			{
+				endingFlag = new Sprite(false, true, location, flagSprites, 1, 50, 0, 50);
+				return endingFlag;
+			}
+			else return endingFlag;
+		}
+
+	}
+}
