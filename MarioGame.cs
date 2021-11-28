b45f9113af0e40c415ba7bf7acde2530cbd35ec3
Jesse Fowler <fowler.514@buckeyemail.osu.edu>
2021-11-27 22:18:56 -0500

    Chunk 6 completed.

--- MarioGame.cs
+++ MarioGame.cs
@@ -39,7 +39,7 @@ namespace Game1
         private ActiveChunkContainer chunks;
         private ChunkParser chunkParser;
         private int marioHeightToLoadNextChunk = 300;
-        private readonly int numberOfChunksInLevelDefinition = 5;
+        private readonly int numberOfChunksInLevelDefinition = 6;
         private int previousChunkId = 1;
 
         private bool playedWarningSound = false;
@@ -350,9 +350,9 @@ namespace Game1
         private void AddRandomNewChunk()
         {
             Random random = new Random();
-            int randomChunkId = random.Next(5, numberOfChunksInLevelDefinition + 1);
+            int randomChunkId = random.Next(6, numberOfChunksInLevelDefinition + 1);
 
-            // TODO - uncomment this and change 5 above to 2 after testing
+            // TODO - uncomment this and change 6 above to 2 after testing
 
             /*while (randomChunkId == previousChunkId)
             {
